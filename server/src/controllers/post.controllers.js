761142b0974b6c0a8dc26947a39ac25c17b16e1b
Juan-David-Orozco <ing.ele2018@gmail.com>
2022-05-24 12:04:46 -0500

    capturando errores - try/catch

--- server/src/controllers/post.controllers.js
+++ server/src/controllers/post.controllers.js
@@ -1,48 +1,57 @@
+import { json } from 'express'
 import Post from '../models/Post.js'
 
 export const getPosts = async (req, res) => {
-  const posts = await Post.find()
-  return res.json(posts)
+  try {
+    //throw new Error('my new error!!!')
+    const posts = await Post.find()
+    return res.json(posts)
+  } catch (error) {
+    console.error(error.message)
+    return res.status(500).json({message: error.message})
+  }
 }
 
 export const createPost = async (req, res) => {
-  const {title, description} = req.body
-  const newPost = new Post({title, description})
-  await newPost.save()
-  return res.json(newPost)
+  try {
+    const {title, description} = req.body
+    const newPost = new Post({title, description})
+    await newPost.save()
+    return res.json(newPost)
+  } catch (error) {
+    console.error(error.message)
+    return res.status(500).json({message: error.message})
+  }
 }
 
 export const updatePost = async (req, res) => {
   try {
-    console.log(req.body, req.params)
     const updatePost = await Post.findByIdAndUpdate(req.params.id, req.body, {new: true})
-    console.log(updatePost)
     return res.json(updatePost)
   } catch (error) {
-    console.log(error.mensaje)
-    return res.json("error.mensaje")
+    console.error(error.message)
+    return res.status(500).json({message: error.message})
   }
 }
 
 export const deletePost = async (req, res) => {
-  const removePost = await Post.findByIdAndDelete(req.params.id)
-  console.log(removePost)
-  if(!removePost) return res.sendStatus(404)
-  return res.sendStatus(204)
+  try {
+    const removePost = await Post.findByIdAndDelete(req.params.id)
+    if(!removePost) return res.sendStatus(404)
+    return res.sendStatus(204)
+  } catch (error) {
+    console.error(error.message)
+    return res.status(500).json({message: error.message})
+  }
 }
 
 export const getPost = async (req, res) => {
-  const post = await Post.findById(req.params.id)
-  if (!post) return res.sendStatus(404)
-  return res.json(post)
-  /*
   try {
     const post = await Post.findById(req.params.id)
-    console.log(post)
+    if (!post) return res.sendStatus(404)
     return res.json(post)
   } catch (error) {
-    console.log(error.mensaje)
-    return res.sendStatus(404)
+    console.error(error.message)
+    return res.status(500).json({message: error.message})
   }
-  */
 }
