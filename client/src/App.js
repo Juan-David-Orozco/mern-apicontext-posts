f89f0488402cd88afa9b2d1d5176ebe7e2db9d58
Juan-David-Orozco <ing.ele2018@gmail.com>
2022-05-25 11:49:36 -0500

    configuracion React -  estructura y modulos

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import {HomePage, PostForm, NotFoundPage} from './pages/indexPages'
+import { Route, Routes } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-neutral-900 min-h-screen flex items-center">
+      <div className="px-10 container bg-red-100 m-auto rounded">
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/new" element={<PostForm/>}/>
+          <Route path="*" element={<NotFoundPage/>}/>
+        </Routes>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ client/src/pages/NotFoundPage.js
@@ -0,0 +1,5 @@
+export function NotFoundPage() {
+  return (
+    <div>NotFoundPage</div>
+  )
+}
