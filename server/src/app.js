0e646b16fa066c9685ec96e0f64eb53d312b2cea
Juan-David-Orozco <ing.ele2018@gmail.com>
2022-06-03 22:38:48 -0500

    desplegando app - parte 1

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import fileUpload from 'express-fileupload'
 import postRoutes from './routes/posts.routes.js'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // Middlewares
 app.use(express.json())
@@ -13,5 +16,8 @@ app.use(fileUpload({
 
 // Routes
 app.use(postRoutes)
+
+console.log(__dirname)
+app.use(express.static(join(__dirname, '../client/build')))
 
 export default app
